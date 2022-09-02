e9fa396c8bee45e39356bf8dea2ed6bb4dabc5d2
mlabastide <102319516+mlabastide@users.noreply.github.com>
2022-09-02 08:39:19 -0400

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+## Are you running this script as root?
+if [[ "${UID}" -ne 0 ]]
+then
+ echo 'Please run this script as "root"' >&2
+ exit 1
+fi
+
+##  System Update
+apt update && apt upgrade -y
+
+echo 'System is up to date'
+
+## Install and Enable OpenSSH
+apt install openssh-server -y
+
+## Install and Enable firewall
+apt install ufw -y
+ufw enable
+ufw allow 22
+echo 'Firewall Enabled'
+
+## Install Fail2Ban
+apt install fail2ban -y
+systemctl start fail2ban
+systemctl enable fail2ban
+
+echo "
+[sshd]
+enabled = true
+port = 22
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 4
+" >> /etc/fail2ban/jail.local
+
+## Create new user with needed directories
+adduser mlab
+usermod -G sudo test
+mkdir /home/mlab/Documents
+mkdir /home/mlab/Downloads
+
+echo 'sudo user created'
+
+## Unattended Upgrades
+sudo apt-get install -y unattended-upgrades
+echo "Unattended-Upgrade::Allowed-Origins {
+#   "${distro_id}:${distro_codename}-security";
+#//  "${distro_id}:${distro_codename}-updates";
+#//  "${distro_id}:${distro_codename}-proposed";
+#//  "${distro_id}:${distro_codename}-backports";
+#Unattended-Upgrade::Automatic-Reboot "true";
+#}; " >> /etc/apt/apt.conf.d/50unattended-upgrades
+
+## Most used Packages installation
+PACKAGES="mlocate neofetch"
+
+apt install $PACKAGES -y
+
+## Clean up
+apt autoremove -y
