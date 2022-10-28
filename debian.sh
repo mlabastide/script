ad09611fc1fb3352843d36459bb0501838b6b195
mlabastide <admin@mlabastide.com>
2022-10-27 21:14:16 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -6,8 +6,11 @@ then
  echo 'Please run this script as "root"' >&2
  exit 1
 fi
+## Install Required Packages
+apt install curl -y
 
 ## Fetch Packages
+
 #Tailscale
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
@@ -18,20 +21,35 @@ curl -LO https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_0
 apt update -y
 
 echo 'System is up to date'
-
-## Install and Enable OpenSSH
+echo "Would you like to install OpenSSH? Y/n/"
+read n
+if [[ ( $n == "y" || $n == "Y" ) ]]
+then
 apt install openssh-server -y
+fi
 
 ## Install and Enable firewall
+echo "Would you like to install a Firewall?"
+read Y
+if [[ ( $Y == "Y" || $Y == "y" ) ]]
+then
 apt install ufw -y
-ufw allow 22
+echo "What ports would you like opened"
+read ports
+ufw allow $ports
 ufw --force enable
-echo 'Firewall Enabled'
+ufw status
+fi
 
 ## Install Fail2Ban
+echo "Would you like to install Fail2Ban?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
 apt install fail2ban -y
-systemctl start fail2ban
 systemctl enable fail2ban
+systemctl start fail2ban
+fi
 
 echo "
 [sshd]
@@ -42,35 +60,134 @@ logpath = /var/log/auth.log
 maxretry = 4
 " >> /etc/fail2ban/jail.local
 
-## Unattended Upgrades
-sudo apt-get install -y unattended-upgrades
-echo "Unattended-Upgrade::Allowed-Origins {
-#   "${distro_id}:${distro_codename}-security";
-#//  "${distro_id}:${distro_codename}-updates";
-#//  "${distro_id}:${distro_codename}-proposed";
-#//  "${distro_id}:${distro_codename}-backports";
-#Unattended-Upgrade::Automatic-Reboot "true";
-#}; " >> /etc/apt/apt.conf.d/50unattended-upgrades
+## Install Tailscale
+echo "Would you like to install tailscale?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install tailscale -y
+fi
 
-## Install Bottom & Tailscale
-apt install tailscale
+## Install Bottom
+echo "Would you like to install bottom system monitor?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
 dpkg -i bottom_0.6.3_amd64.deb
+fi
 
+## Install neofetch
+echo "Would you like to install neofetch ?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install neofetch -y
+fi
 
-## Common Packages
-PACKAGES="mlocate neofetch speedtest-cli rclone fzf ranger thefuck nano xsensors net-tools"
+## Install mlocate
+echo "Would you like to install mlocate ?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install mlocate -y
+update db
+fi
 
-apt install $PACKAGES -y
+## Install speedtest-cli
+echo "Would you like to install speedtest?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install speedtest-cli -y
+speedtest
+fi
 
-## Create new user with needed directories
-adduser mlabastide
-usermod -G sudo mlabastide
-mkdir /home/mlabastide/Documents
-mkdir /home/mlabastide/Downloads
+## Install rclone
+echo "Would you like to install rclone?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install rclone -y
+fi
 
-echo 'sudo user created'
+## Install ncdu 
+echo "Would you like to install NCurses DisK Usage?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install ncdu -y
+fi
 
-## Upgrade
+## Install ranger
+echo "Would you like to install ranger CLI file manager?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install ranger -y
+fi
+
+## Install fuck
+echo "Would you like to install fuck?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install thefuck -y
+fi
+
+## Install nano
+echo "Would you like to install nano?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install nano -y
+fi
+
+## Install xsensors
+echo "Would you like to install xsensors?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install xsensors -y
+fi
+
+## Install net-tools
+echo "Would you like to install net tools?"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+apt install net-tools -y
+fi
+
+## new-user
+echo "Let's setup a user?"
+sleep 1
+echo "Pleae enter a username"
+read user
+adduser $user
+wait
+mkdir /home/$user/Documents
+mkdir /home/$user/Downloads
+echo "Should this user be sudo?"
+read y
+if [[ ( $y == "Y" || $y == "y" ) ]]
+then
+usermod -G sudo $user
+echo "$user has been created and added to the sudo group"
+else
+echo "$user has been created with restricted access"
+fi
+
+echo "One last thing, We need to update the system and clean up some installation files"
+sleep 2
 apt upgrade -y
-## Clean up
 apt autoremove -y
+apt-get autoremove -y
+
+echo "We must reboot, would you like to do this now"
+read y
+if [[ ( $y == "y" || $y == "Y" ) ]]
+then
+reboot now
+else
+echo "You really should reboot now....."
+fi
